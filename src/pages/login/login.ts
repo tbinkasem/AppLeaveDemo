71b764f171ee205c17ecf8fdb6e1cf0a2d03c774
bteera <tbinkasem@ptnicec.ac.th>
2018-09-28 10:55:14 +0700

    update 7

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { LoginServiceProvider } from '../../providers/login-service/login-service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { HomePage } from '../home/home';
 
 @IonicPage()
@@ -11,27 +12,46 @@ import { HomePage } from '../home/home';
 })
 export class LoginPage {
 
-  username = "";
-  password= "";
-  item: any;
+  model: Array<any>;
+  formLogin: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-              public loginService: LoginServiceProvider,
-              public alertControl: AlertController,
-  ) {
-    
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public loginservice: LoginServiceProvider,
+              private fb: FormBuilder,
+              public alertCtrl: AlertController
+              ) {
+                this.formLogin = this.fb.group({
+                  username: ["", Validators.required],
+                  password: ["",Validators.required]
+                });
   }
 
-  disMiss(){
-    this.navCtrl.setRoot(HomePage);
-  }
+  doLogin(){
+    let username = this.formLogin.controls['username'].value;
+    let password = this.formLogin.controls['password'].value;
 
-  doLogin(event, username, password){
-    this.loginService.doLogin(username,password).subscribe(
-      data =>{
-        this.item = data.message;
-        console.log("item", + this.item);
+    this.model = null;
+    this.loginservice.doLogin(username, password).subscribe(
+      data => {
+        if(data.toString() == 'denied'){
+          this.alertCtrl.create({
+            title: "Permission",
+            message: "Not Permission"
+          });
+        }else{
+          this.navCtrl.push(HomePage);
+        }
+      },
+      error=>{
+        let alert = this.alertCtrl.create({
+          title: "ERROR!!",
+          message: "Network Error!!",
+          buttons: ["OK"]
+        });
+        alert.present();
       }
     )
   }
+
 }
